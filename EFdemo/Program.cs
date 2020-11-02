fede9068af393d16075c6c9f3dae6913a3905e7a
MikeZorin <mykhailo.zorin@gmail.com>
2020-11-02 13:05:42 -0600

    Add project files.

---
+++ EFdemo/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EFdemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
